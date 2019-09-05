de9f73f9520ed96d8b32637522cd5e0cc105c1d9
Daniel Furman <djfurman@gmail.com>
2019-09-05 07:35:08 -0400

    Add contact basic model

---
+++ models/contact.go
@@ -0,0 +1,74 @@
+package models
+
+import (
+	"fmt"
+	u "go-contacts/utils"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Contact struct {
+	gorm.Model
+	Name   string `json:"name"`
+	Phone  string `json:"phone"`
+	UserId uint   `json:"user_id"` // The user that this contact belongs to
+}
+
+/*
+	Struct function validates the model for requiremed parameters
+	returns message and true if the requirement is met
+*/
+func (contact *Contact) Validate() (map[string]interface{}, bool) {
+
+	if contact.Name == "" {
+		return u.Message(false, "Contact name should be on the payload"), false
+	}
+
+	if contact.Phone == "" {
+		return u.Message(false, "Phone number should be on the payload"), false
+	}
+
+	if contact.UserId <= 0 {
+		return u.Message(false, "User is not recognized"), false
+	}
+
+	// All the required parameters are present
+	return u.Message(true, "success"), true
+}
+
+func (contact *Contact) Create() map[string]interface{} {
+
+	if resp, ok := contact.Validate(); !ok {
+		return resp
+	}
+
+	GetDB().Create(contact)
+
+	resp := u.Message(true, "success")
+	resp["contact"] = contact
+	return resp
+}
+
+func GetContact(user uint) *Contact {
+
+	contacts := &Contact{}
+	err := GetDB().Table("contacts").Where("user_id = ?", user).Find(&contacts).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return contacts
+}
+
+func GetContacts(user uint) []*Contact {
+
+	contacts := make([]*Contact, 0)
+	err := GetDB().Table("contacts").Where("user_id = ?", user).Find(&contacts).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return contacts
+}
